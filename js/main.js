5b824837b2f039388c49fbe09da0d4745fdbc16d
Nick Langer <info@nicklanger.de>
2019-02-19 09:23:56 +0100

    experimenting with the live search

--- js/main.js
+++ js/main.js
@@ -1,13 +1,40 @@
-document.getElementById("search").addEventListener("keyup", myFunction);
-var x = "";
-function myFunction() {
-  x = document.getElementById("search");
-  x = x.value.toLowerCase();
-  return x
 
+// Catching the input of the searchfield //
+
+document.getElementById("search").addEventListener("keyup", myFunction);
+
+function myFunction() {
+  
+  let x = document.getElementById("search");
+  x = x.value.toLowerCase();
+  console.log(x);
+
+  // var y = document.getElementsByTagName("a");
+  // for (i = 0; i < y.length; i++) {
+  //   let dataTitles = y[i].getAttribute("data-title").toLowerCase();
+    
+  //   if (dataTitles === null) {
+  //   console.log("dataTitles");
+  // }
+      
+    
+  //   }
+    
+    
 }
 
-console.log(myFunction());
+
+
+// Looping through the data-titles and logging them to the console //
+
+// var y = document.getElementsByTagName("a")[0].getAttribute("data-title");
+// console.log(y);
+var y = document.getElementsByTagName("a");
+
+for (i = 0; i < y.length; i++) {
+  let dataTitles = y[i].getAttribute("data-title").toLowerCase()
+  console.log(dataTitles);
+}
 
 
 
