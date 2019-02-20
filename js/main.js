53546a60189f8bed10f6da099a0202f8b931e7d5
Nick Langer <info@nicklanger.de>
2019-02-20 10:14:33 +0100

    Add media queries for navigation arrows and formatting the files

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 const input = document.getElementById("search"); // Getting the input field
 const entries = document.getElementsByClassName("entry"); // Getting all a elements with the class of "entry"
 
-input.addEventListener("keyup", function(){ // Adds keyup eventlistener to the input
+input.addEventListener(
+  "keyup",
+  function() {
+    // Adds keyup eventlistener to the input
     const val = input.value; // Getting the user input over the input field
-    for (let i = 0; i < entries.length; i++) { // Looping over all entries
-        const caption = entries[i].getAttribute("data-title"); // Getting the data-title value
-        if (caption.toLowerCase().indexOf(val.toLowerCase()) != -1) { // 
-        entries[i].style.display = "block"; 
-    }else entries[i].style.display = "none";
+    for (let i = 0; i < entries.length; i++) {
+      // Looping over all entries
+      const caption = entries[i].getAttribute("data-title"); // Getting the data-title value
+      if (caption.toLowerCase().indexOf(val.toLowerCase()) != -1) {
+        //
+        entries[i].style.display = "block";
+      } else entries[i].style.display = "none";
     }
-}, false)
-
+  },
+  false
+);
